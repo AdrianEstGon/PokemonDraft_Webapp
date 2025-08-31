679d8875f1c16962ea5dbd56e1a7b8c567e39d00
Adrián Estrada González <adrian.estrada2001@gmail.com>
2025-08-31 17:36:51 +0200

    url api

--- src/utils/config.ts
+++ src/utils/config.ts
@@ -1,2 +1,2 @@
 // Configuración global de la API
-export const API_BASE_URL = "https://voleyasturias-001-site7.jtempurl.com/api"; // 👈 cambia si tu puerto/base cambia
+export const API_BASE_URL = "https://pokemondraftapi-3.onrender.com/api"; // 👈 cambia si tu puerto/base cambia
