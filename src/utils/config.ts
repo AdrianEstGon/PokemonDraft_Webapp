f70335dc9c26c1c7d9cf703ee63be103fb1d4eb5
Adrián Estrada González <adrian.estrada2001@gmail.com>
2025-08-31 04:14:29 +0200

    https

--- src/utils/config.ts
+++ src/utils/config.ts
@@ -1,2 +1,2 @@
 // Configuración global de la API
-export const API_BASE_URL = "http://voleyasturias-001-site7.jtempurl.com/api"; // 👈 cambia si tu puerto/base cambia
+export const API_BASE_URL = "https://voleyasturias-001-site7.jtempurl.com/api"; // 👈 cambia si tu puerto/base cambia
